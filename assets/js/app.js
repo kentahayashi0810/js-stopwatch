8487253dc2e8832f70c228cc58e0d2ae9569a9a4
kentahayashi <kenta0810hayashi@gmail.com>
2023-04-16 00:45:02 +0900

    Added js vriables

---
+++ assets/js/app.js
@@ -0,0 +1,52 @@
+window.onload = function () {
+  let seconds = 00;
+  let tens = 00;
+
+  const appendSeconds = document.getElementById("seconds");
+  const appendTens = document.getElementById("tens");
+  const buttonPause = document.getElementById("button-pause");
+  const buttonStart = document.getElementById("button-start");
+  const buttonReset = document.getElementById("button-reset");
+
+  let interval;
+
+  buttonStart.onclick = function () {
+    clearInterval(interval);
+    interval = setInter(startTimer, 10);
+  };
+
+  buttonReset.onclick = function () {
+    clearInterval(interval);
+  };
+
+  buttonReset.onclick = function () {
+    clearInterval(interval);
+    seconds = "00";
+    tens = "00";
+    appendSeconds.innerHTML = seconds;
+    appendTens.innerHTML = tens;
+  };
+};
+
+function startTimer() {
+  tens++;
+
+  if (tens < 10) {
+    appendTens.innerHTML = "0" + tens;
+  }
+
+  if (tens > 9) {
+    appendTens.innerHTML = tens;
+  }
+
+  if (tens > 99) {
+    seconds++;
+    appendSeconds.innerHTML = "0" + seconds;
+    tens = 0;
+    appendTens.innerHTML = "0" + tens;
+  }
+
+  if (seconds > 9) {
+    appendSeconds.innerHTML = seconds;
+  }
+}
